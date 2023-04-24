a77d47d46a388c099c00b41f88d426eba5194824
RobinFritzFredriksson <you@example.com>
2023-04-24 15:57:33 +0200

    RegisterDto Update

--- Manero-backend/DTOs/User/RegisterDto.cs
+++ Manero-backend/DTOs/User/RegisterDto.cs
@@ -2,5 +2,10 @@
 {
     public class RegisterDto
     {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
     }
 }
