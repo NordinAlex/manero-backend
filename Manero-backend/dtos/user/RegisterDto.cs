6a11becd7766afb9dc57f571a8c6670fb25018b9
RobinFritzFredriksson <you@example.com>
2023-04-24 15:42:09 +0200

    RegisterDTO och UserDTO uppsatta.

---
+++ Manero-backend/dtos/user/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Manero_backend.DTOs.User
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
---
+++ Manero-backend/dtos/user/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Manero_backend.DTOs.User
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
