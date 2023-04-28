84c78e29059e143b2e22eadfbe2fe21f5330441c
AlexNordin-dev <alexnordin.dev@hotmail.com>
2023-04-28 23:51:13 +0200

    Interface for Service has been added

---
+++ Manero-backend/Interfaces/Product/Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Manero_backend.DTOs.Product;
+
+namespace Manero_backend.Interfaces.Product.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductAsync();
+        Task<ProductDto> GetProductByIdAsync(int id);
+        Task<IEnumerable<ProductDto>> GetProductByTypeIdAsync(int TypeId);
+        Task<ProductDto> CreateProductAsync(CreateProductDto productRequest);
+        Task<ProductDto> UpdateProductAsync(int id, CreateProductDto productRequest);
+        Task DeleteProductAsync(int id);
+    }
+}
