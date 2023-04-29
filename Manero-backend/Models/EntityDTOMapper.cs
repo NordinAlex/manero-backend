c42f9cb66bad6533a6c2ebebb067262762893054
AlexNordin-dev <alexnordin.dev@hotmail.com>
2023-04-29 17:36:53 +0200

    Add EntityDTOMapper

---
+++ Manero-backend/Models/EntityDTOMapper.cs
@@ -0,0 +1,33 @@
+﻿using Azure;
+using Manero_backend.DTOs.Product;
+using Manero_backend.Models.ProductEntities;
+
+namespace Manero_backend.Models
+{
+    public static class EntityDTOMapper
+    {
+        public static ProductResponse ToProductResponse(this ProductEntity product,
+            IEnumerable<BrandEntity> brands, IEnumerable<ColorEntity> colors, IEnumerable<ImagesEntity> images,
+            IEnumerable<SizeEntity> sizes, IEnumerable<TagsEntity> tags, IEnumerable<TypeEntity> types)
+        {
+            return new ProductResponse
+            {
+
+            };
+        }
+        public static ProductEntity ToProductEntity(this ProductRequest request)
+        {
+
+            return new ProductEntity
+            {
+
+
+            };
+        }
+
+        public static void UpdateProductEntity(this ProductRequest request, ProductEntity entity)
+        {
+
+        }
+    }
+}
