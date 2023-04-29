b79f0a267cc8d2640f6363402ca7ade1b0898d76
AlexNordin-dev <alexnordin.dev@hotmail.com>
2023-04-29 16:21:19 +0200

    Update

---
+++ Manero-backend/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Manero_backend.Interfaces.Product.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manero_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        // GET: api/Product
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
--- Manero-backend/Services/ProductService.cs
+++ Manero-backend/Services/ProductService.cs
@@ -1,6 +1,64 @@
-﻿namespace Manero_backend.Services
+﻿using Manero_backend.DTOs.Product;
+using Manero_backend.Interfaces.Product;
+using Manero_backend.Interfaces.Product.Repositories;
+using Manero_backend.Interfaces.Product.Services;
+
+namespace Manero_backend.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly IProductRepository _productRepository;
+        private readonly IBrandRepository _brandRepository;
+        private readonly IColorRepository _colorRepository;
+        private readonly IImageRepository _imageRepository;
+        private readonly ISizeRepository _sizeRepository;
+        private readonly ITagRepository _tagRepository;
+        private readonly ITypeRepository _typeRepository;
+
+        public ProductService(IProductRepository productRepository, IBrandRepository brandRepository, IColorRepository colorRepository, IImageRepository imageRepository, ISizeRepository sizeRepository, ITagRepository tagRepository, ITypeRepository typeRepository)
+        {
+            _productRepository = productRepository;
+            _brandRepository = brandRepository;
+            _colorRepository = colorRepository;
+            _imageRepository = imageRepository;
+            _sizeRepository = sizeRepository;
+            _tagRepository = tagRepository;
+            _typeRepository = typeRepository;
+        }
+
+        public Task<ProductResponse> CreateProductAsync(ProductRequest productRequest)
+        {
+            // Alex
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int id)
+        {
+            // Ilona
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductResponse>> GetAllProductAsync()
+        {
+            // Julius
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductResponse> GetProductByIdAsync(int id)
+        {
+            // Julius
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductResponse>> GetProductByTypeIdAsync(int TypeId)
+        {   // Julius
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductResponse> UpdateProductAsync(int id, ProductRequest productRequest)
+        {
+            // Ilona
+            throw new NotImplementedException();
+        }
     }
 }
