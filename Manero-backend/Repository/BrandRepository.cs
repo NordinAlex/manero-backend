afc7809030ba1ca258986865af2aecab53c24173
AlexNordin-dev <alexnordin.dev@hotmail.com>
2023-04-29 11:18:40 +0200

    Add Repository

--- Manero-backend/Repository/BrandRepository.cs
+++ Manero-backend/Repository/BrandRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace Manero_backend.Repository
+﻿using Manero_backend.Context;
+using Manero_backend.Interfaces.Product.Repositories;
+using Manero_backend.Models.ProductEntities;
+
+namespace Manero_backend.Repository
 {
-    public class BrandRepository
+    public class BrandRepository : IBrandRepository
     {
+        private readonly DataContext _context;
+
+        public BrandRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<BrandEntity>> GetAllBrandAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
