0186f5c2baa1bfeeed4c1d6741aac5c7f2cef046
Oscar Enbacka <enbacka.oscar@gmail.com>
2023-05-19 21:54:17 +0200

    Services

---
+++ Manero-backend/Factories/CartFactories/ShoppingCartServiceFactory.cs
@@ -0,0 +1,23 @@
+﻿using Manero_backend.Interfaces.Cart;
+using Manero_backend.Services.CartRelatedServices;
+
+namespace Manero_backend.Factories.CartRelatedFactories
+{
+    //Oscar, Belal
+    public class ShoppingCartServiceFactory : IShoppingCartServiceFactory
+    {
+        private readonly ICartRepository _cartRepository;
+        //  private readonly IMapper _mapper;
+
+        public ShoppingCartServiceFactory(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+            // _mapper = mapper;
+        }
+
+        public IShoppingCartService CreateShoppingCartService()
+        {
+            return new ShoppingCartService(_cartRepository);
+        }
+    }
+}
---
+++ Manero-backend/Interfaces/Cart/IShoppingCartService.cs
@@ -0,0 +1,17 @@
+﻿using Manero_backend.Models.CartDto;
+
+namespace Manero_backend.Interfaces.Cart
+{
+    //Oscar, Belal
+    //Flytta denna till Interface Mappen
+    public interface IShoppingCartService
+    {
+        Task<int> CreateCartAsync(string email);
+        Task<CartResponseDTO> GetCartByEmailAsync(string email);
+
+        Task<CartItemResponseDTO> AddCartItemAsync(int cartId, CartItemRequestDTO cartItemDto);
+        Task<CartItemResponseDTO> UpdateCartItemAsync(int cartItemId, CartItemRequestDTO cartItemDto);
+        Task RemoveCartItemAsync(int cartItemId);
+        Task<int> ClearCartAsync(int cartId);
+    }
+}
