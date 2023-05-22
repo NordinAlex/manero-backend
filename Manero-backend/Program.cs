a49c4b8a799a3cc7de3b342a6545f801c878b8f6
Patrik Byren <patrik.byren@hotmail.com>
2023-05-22 19:44:30 +0200

    fixed errors in program

--- Manero-backend/Program.cs
+++ Manero-backend/Program.cs
@@ -1,6 +1,8 @@
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Manero_backend.Context;
+using Manero_backend.Factories.CartRelatedFactories;
+using Manero_backend.Interfaces.Addresses.Repository;
 using Manero_backend.Interfaces.Addresses.Service;
 using Manero_backend.Interfaces.Cart;
 using Manero_backend.Interfaces.Order;
