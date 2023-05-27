8fce78eb33340caddf6b250f47c916daa3c1d43d
Patrik Byren <patrik.byren@hotmail.com>
2023-05-27 19:45:53 +0200

    xUnit setup

---
+++ Manero-backend-tests/Fixtures/AddressFixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manero_backend_tests.Fixtures
+{
+    internal class AddressFixture
+    {
+        /*
+        public AddressRequest ValidAddressRequest { get; }
+        public AddressRequest InvalidAddressRequest { get; }
+        public AddressResponse ValidAddressResponse { get; }
+        public AddressResponse InvalidAddressResponse { get; }
+    
+        public AddressFixture()
+        {
+            ValidAddressRequest = new AddressRequest
+            {
+                TagName = "Home",
+                StreetName = "123 Main St",
+                PostalCode = "12345",
+                City = "City",
+                Email = "test@example.com",
+                BillingAddress = true
+            };
+
+            InvalidAddressRequest = new AddressRequest
+            {
+
+            };
+
+            ValidAddressResponse = new AddressResponse
+            {
+                StreetName = "123 Main St",
+                City = "City",
+                PostalCode = "12345",
+
+            };
+
+            InvalidAddressResponse = new AddressResponse
+            {
+
+            };
+        }*/
+    }
+}
+
---
+++ Manero-backend-tests/UnitTests/AddressController_Tests.cs
@@ -0,0 +1,11 @@
+namespace Manero_backend_tests.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
