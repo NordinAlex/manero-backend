62d0b6abf3936eec323dc6a06b6268ed9a072bc8
Patrik Byren <patrik.byren@hotmail.com>
2023-05-27 20:02:36 +0200

    Fixed in address fixture

--- Manero-backend-tests/Fixtures/AddressFixture.cs
+++ Manero-backend-tests/Fixtures/AddressFixture.cs
@@ -1,49 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Manero_backend_tests.Fixtures
 {
-    internal class AddressFixture
+    public class AddressFixture
     {
-        /*
-        public AddressRequest ValidAddressRequest { get; }
-        public AddressRequest InvalidAddressRequest { get; }
-        public AddressResponse ValidAddressResponse { get; }
-        public AddressResponse InvalidAddressResponse { get; }
-    
+        
+        public AddressRequest ValidAddressCreateRequest { get; }
+        public AddressRequest InvalidAddressCreateRequest { get; }
+        public AddressResponse ValidAddressCreateResponse { get; }
+        public AddressResponse InvalidAddressCreateResponse { get; }
+        public AddressResponse ValidGetAllAddressForOneUserResponse { get; }
+
+        public class AddressRequest
+        {
+            public string? TagName { get; set; }
+            public string? StreetName { get; set; } 
+            public string? PostalCode { get; set; }
+            public string? City { get; set; }
+            public string? Email { get; set; }
+            public bool BillingAddress { get; set; }
+        }
+        public class AddressResponse
+        {
+            public string? StreetName { get; set; }
+            public string? City { get; set; }
+            public string? PostalCode { get; set; }
+            public string? StatusMessage { get; set; }
+            public bool Error { get; set; }
+            public string? TagName { get; set; }
+            public bool BillingAddress { get; set; }
+            public bool Active { get; set; }
+            public ICollection<AddressResponse>? AddressList { get; set; }
+        }
         public AddressFixture()
         {
-            ValidAddressRequest = new AddressRequest
+            ValidAddressCreateRequest = new AddressRequest
             {
                 TagName = "Home",
                 StreetName = "123 Main St",
                 PostalCode = "12345",
                 City = "City",
-                Email = "test@example.com",
                 BillingAddress = true
             };
 
-            InvalidAddressRequest = new AddressRequest
+            InvalidAddressCreateRequest = new AddressRequest
             {
 
             };
 
-            ValidAddressResponse = new AddressResponse
+            ValidAddressCreateResponse = new AddressResponse
             {
                 StreetName = "123 Main St",
                 City = "City",
                 PostalCode = "12345",
+                Active = true,
+                TagName = "Home",
+                BillingAddress = true
 
             };
 
-            InvalidAddressResponse = new AddressResponse
+            InvalidAddressCreateResponse = new AddressResponse
             {
+                StatusMessage = "Error",
+                Error = true
+            };
+            ValidGetAllAddressForOneUserResponse = new AddressResponse
+            {
+                AddressList = new List<AddressResponse>
+                {
+                    new AddressResponse
+                    {
+                        StreetName = "123 Main St 1",
+                        City = "City 1",
+                        PostalCode = "12345",
+                        Active = true,
+                        TagName = "Home",
+                        BillingAddress = true
+                    },
+                    new AddressResponse
+                    {
+                        StreetName = "123 Main St 2",
+                        City = "City 2",
+                        PostalCode = "12345",
+                        Active = true,
+                        TagName = "Work",
+                        BillingAddress = false
+                    }
+                }
 
             };
-        }*/
+        }
     }
 }
 
